8c75e3ea898ec79efa5833494d76671e40220dde
Ahmed Shalaby <89112494+Shalaby000@users.noreply.github.com>
2026-05-24 06:55:37 +0300

    UI_FIX

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ function uid() {
 }
 
 function getApiKey() {
-  return 'AIzaSyAZUCqqyzKfbFLyRP7qOasCGTgsA65tyy0';
+  return localStorage.getItem(API_KEY_STORE) || '';
 }
 
 function detectType(src) {
